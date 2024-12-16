6ac2d90678c0a00955a27681be1b88f7dd4b4059
Marcello Rego <marcellorego@gmail.com>
2024-12-16 22:05:58 +0000

    Add MapperConfiguration configuration

---
+++ src/main/java/com/example/demo/config/MapperConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.demo.config;
+
+import com.example.demo.mapper.AssetMapper;
+import com.example.demo.mapper.WalletMapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This class configures the mappers
+ */
+
+@Configuration
+public class MapperConfiguration {
+
+    /**
+     * Asset mapper asset mapper.
+     *
+     * @return the asset mapper
+     */
+    @Bean
+    public AssetMapper assetMapper() {
+        return Mappers.getMapper(AssetMapper.class);
+    }
+
+    /**
+     * Wallet mapper wallet mapper.
+     *
+     * @return the wallet mapper
+     */
+    @Bean
+    public WalletMapper walletMapper() {
+        return Mappers.getMapper(WalletMapper.class);
+    }
+}
