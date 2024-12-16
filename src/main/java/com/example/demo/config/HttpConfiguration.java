2140856089dd0570c10755e3f05a8ca4ad811d53
Marcello Rego <marcellorego@gmail.com>
2024-12-16 22:05:19 +0000

    Add HttpConfiguration configuration

---
+++ src/main/java/com/example/demo/config/HttpConfiguration.java
@@ -0,0 +1,54 @@
+package com.example.demo.config;
+
+import com.example.demo.config.properties.CoinCapProperties;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+/**
+ * This class configures the HTTP client
+ */
+
+@Configuration
+public class HttpConfiguration {
+
+    private final CoinCapProperties coinCapProperties;
+
+    public HttpConfiguration(final CoinCapProperties coinCapProperties) {
+        this.coinCapProperties = coinCapProperties;
+    }
+
+    @Bean
+    public CloseableHttpClient closeableHttpClient() {
+        return HttpClientBuilder.create().build();
+    }
+
+    @Bean
+    public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory(final CloseableHttpClient httpClient) {
+
+        final HttpComponentsClientHttpRequestFactory clientHttpRequestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setHttpClient(httpClient);
+        clientHttpRequestFactory.setConnectionRequestTimeout(coinCapProperties.getReadTimeout());
+        clientHttpRequestFactory.setConnectTimeout(coinCapProperties.getConnectionTimeout());
+        return clientHttpRequestFactory;
+    }
+
+    @Bean("coinCapRestClient")
+    public RestClient restClient(final HttpComponentsClientHttpRequestFactory clientHttpRequestFactory) {
+
+        return RestClient.builder()
+                .baseUrl(coinCapProperties.getUrl())
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.add("Authorization",
+                            "Bearer " + coinCapProperties.getApiKey());
+                    httpHeaders.add("Accept", "application/json");
+                    httpHeaders.add("Content-Type", "application/json");
+                })
+                .requestFactory(clientHttpRequestFactory)
+                .build();
+    }
+}
