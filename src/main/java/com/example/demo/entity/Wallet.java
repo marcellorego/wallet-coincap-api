7be7d3e6a9373ce51bed132de0c2dd198274b863
Marcello Rego <marcellorego@gmail.com>
2024-12-17 17:34:48 +0000

    Change Wallet entity to hold a set of related assets

--- src/main/java/com/example/demo/entity/Wallet.java
+++ src/main/java/com/example/demo/entity/Wallet.java
@@ -4,12 +4,12 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-import java.io.Serializable;
-import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
- * Wallet performance entity.
+ * Wallet entity.
  */
 
 @Builder
@@ -18,45 +18,26 @@ import java.time.Instant;
 @Entity
 @Table(name = "wallet")
 @AllArgsConstructor
-@EqualsAndHashCode(of = {"walletId"})
+@EqualsAndHashCode(of = {"id"})
 public class Wallet {
 
-    @EmbeddedId
-    private WalletId walletId;
+    // identifier for wallet (CFJK32DUI4)
+    @Id
+    @NotNull(message = "Wallet id is required")
+    @Column(name = "id", updatable = false, nullable = false, length = 10)
+    private String id;
 
-    // 9999999999.00 total financial value in USD
-    @Column(name = "total_usd", nullable = false)
+    // updated timestamp of wallet
+    @Column(name = "updated_at", nullable = false)
     @NotNull
-    private BigDecimal totalUsd;
+    private Instant updatedAt;
 
-    @Column(name = "best_asset", length = 5)
-    private String bestAsset;
-
-    @Column(name = "best_performance")
-    private Float bestPerformance;
-
-    @Column(name = "worst_asset", length = 5)
-    private String worstAsset;
-
-    @Column(name = "worst_performance")
-    private Float worstPerformance;
-
-    @Data
-    @Builder
-    @NoArgsConstructor(force = true)
-    @AllArgsConstructor
-    @EqualsAndHashCode(of = {"id", "createdAt"})
-    @Embeddable
-    public static class WalletId implements Serializable {
-
-        // identifier for account (CFJK32DUI4)
-        @NotNull(message = "Wallet id is required")
-        @Column(name = "id", updatable = false, nullable = false, length = 10)
-        private String id;
-
-        // updated timestamp of asset price
-        @Column(name = "created_at", nullable = false)
-        @NotNull
-        private Instant createdAt;
-    }
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {
+            CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name = "wallet_assets",
+            joinColumns = @JoinColumn(name = "wallet_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "asset_id", referencedColumnName = "id"))
+    private Set<Asset> linkedAssets = new LinkedHashSet<>();
 }
