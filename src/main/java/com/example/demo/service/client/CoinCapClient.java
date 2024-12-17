eef93818e2d1918f44103bd0001e6f11c242d200
Marcello Rego <marcellorego@gmail.com>
2024-12-17 17:31:52 +0000

    Add method `getAssetHistoryByPeriod` Change method `getAssetBySymbol` to `getAssetBySearchId`

--- src/main/java/com/example/demo/service/client/CoinCapClient.java
+++ src/main/java/com/example/demo/service/client/CoinCapClient.java
@@ -40,23 +40,31 @@ public class CoinCapClient {
                 .retrieve()
                 .onStatus(HttpStatusCode::is5xxServerError,
                         (request, response) -> {
-                            throw new CoinCapException("Error requesting url %s".formatted(template), null);
+                            throw new CoinCapException("Server error while requesting url %s".formatted(template),
+                                    response.getStatusCode(),
+                                    null);
+                        })
+                .onStatus(HttpStatusCode::is4xxClientError,
+                        (request, response) -> {
+                            throw new CoinCapException("Client error while requesting to url %s".formatted(template),
+                                    response.getStatusCode(),
+                                    null);
                         })
                 .body(clazz);
     }
 
     /**
-     * Fetches asset by symbol
+     * Fetches asset by search identifier
      *
-     * @param symbol the symbol of the asset
+     * @param searchId the search identifier of the asset
      * @return the asset
      * @throws CoinCapException if there is an error fetching data
      */
-    public Optional<CoinCapAssets> getAssetBySymbol(final String symbol) throws CoinCapException {
+    public Optional<CoinCapAssets> getAssetBySearchId(final String searchId) throws CoinCapException {
 
         final UriComponents uriComponents = UriComponentsBuilder.newInstance()
                 .path("/assets")
-                .queryParam("search", symbol)
+                .queryParam("search", searchId)
                 .queryParam("offset", 0)
                 .queryParam("limit", 1)
                 .build();
@@ -66,11 +74,38 @@ public class CoinCapClient {
             log.debug("Fetched assets from offset {} limit {}: {}", 1, 1, result);
             return Optional.of(result);
         } catch (CoinCapException e) {
-            log.error("Error fetching asset by symbol {}", symbol, e);
+            log.error("Error fetching asset by search value {}", searchId, e);
         }
 
         return Optional.empty();
     }
+
+    /**
+     * Fetches asset history by period
+     *
+     * @param assetId the id of the asset
+     * @param start   the start of the period
+     * @param end     the end of the period
+     * @return the asset history
+     * @throws CoinCapException if there is an error fetching data
+     */
+    public CoinCapHistoryAssets getAssetHistoryByPeriod(final String assetId,
+                                                        final Long start,
+                                                        final Long end,
+                                                        final String interval) throws CoinCapException {
+
+        final UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .path("/assets/{id}/history")
+                .queryParam("start", start)
+                .queryParam("end", end)
+                .queryParam("interval", interval)
+                .buildAndExpand(Map.of("id", assetId));
+
+        final CoinCapHistoryAssets result = get(uriComponents.toUriString(), CoinCapHistoryAssets.class);
+        log.debug("Fetched asset {} history from start {} to end {}: {}", assetId, start, end, result);
+        return result;
+    }
+
 
     /**
      * Fetches assets from CoinCap API
