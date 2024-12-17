e51265d2774cf9b415db5253400046bdae4a55bb
Marcello Rego <marcellorego@gmail.com>
2024-12-17 20:13:17 +0000

    Add `AssetRepositoryTest` with JpaTestProfile and DataJpaTest

--- src/test/java/com/example/demo/test/JpaTestProfile.java
+++ src/test/java/com/example/demo/test/JpaTestProfile.java
@@ -1,5 +1,7 @@
 package com.example.demo.test;
 
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,5 +10,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @TestProfile
+@DataJpaTest
 public @interface JpaTestProfile {
 }
