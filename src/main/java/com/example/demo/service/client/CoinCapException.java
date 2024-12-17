b3ce521b8e8affdc52dd0c05e54a0a5a89b76c66
Marcello Rego <marcellorego@gmail.com>
2024-12-17 17:30:49 +0000

    Add httpStatusCode to CoinCapException

--- src/main/java/com/example/demo/service/client/CoinCapException.java
+++ src/main/java/com/example/demo/service/client/CoinCapException.java
@@ -1,12 +1,19 @@
 package com.example.demo.service.client;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+
 /**
  * CoinCap exception.
  */
 
+@Getter
 public class CoinCapException extends RuntimeException {
 
-  public CoinCapException(String message, Throwable cause) {
+  private final HttpStatusCode statusCode;
+
+  public CoinCapException(String message, HttpStatusCode statusCode, Throwable cause) {
     super(message, cause);
+    this.statusCode = statusCode;
   }
 }
