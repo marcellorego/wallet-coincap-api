9dd58a7805ddf83bdf1a4f71c148f225af373627
Marcello Rego <marcellorego@gmail.com>
2024-12-17 19:43:20 +0000

    Add `JpaTestProfile` annotation for dev profile

---
+++ src/test/java/com/example/demo/test/JpaTestProfile.java
@@ -0,0 +1,12 @@
+package com.example.demo.test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@TestProfile
+public @interface JpaTestProfile {
+}
